d1a7b67393cb4a4ebffd8b424bda9695ada01352
farinhahelder-hue <farinhahelder@gmail.com>
2026-06-19 16:49:40 +0200

    feat: ajout icônes favicon dans metadata layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,18 @@ export const metadata: Metadata = {
   authors: [{ name: 'Monica Schneider', url: SITE_URL }],
   creator: 'Monica Schneider',
   publisher: 'Happy Humans',
+  icons: {
+    icon: [
+      { url: '/favicon.ico' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ],
+    apple: '/apple-touch-icon.png',
+    other: [
+      { rel: 'android-chrome-192x192', url: '/android-chrome-192x192.png' },
+      { rel: 'android-chrome-512x512', url: '/android-chrome-512x512.png' },
+    ],
+  },
   robots: {
     index: true,
     follow: true,
